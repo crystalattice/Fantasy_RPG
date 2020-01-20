b772bf4b43ec78c3d8702d8444b255431bdc76fa
codyjackson <crystalattice@gmail.com>
2020-01-19 21:07:12 -0600

    Added default values

--- Adv_Dark_Deep/Char_Creation/basic_character.py
+++ Adv_Dark_Deep/Char_Creation/basic_character.py
@@ -1,67 +1,67 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import Type, Dict, Any, List
 
 
 @dataclass()
 class Character:
     """Basic character information, common to all player characters"""
-    strength: Type[float]
-    dexterity: Type[int]
-    intelligence: Type[int]
-    wisdom: Type[int]
-    constitution: Type[int]
-    charisma: Type[int]
+    strength: float = 0.0
+    dexterity: int = 0
+    intelligence: int = 0
+    wisdom: int = 0
+    constitution: int = 0
+    charisma: int = 0
 
-    save_vs_ppd: Type[int]  # Save vs. poison, paralyzation, death
-    save_vs_pp: Type[int]  # Save vs. petrification, polymorph
-    save_vs_bw: Type[int]  # Save vs. breath weapon
-    save_vs_rsw: Type[int]  # Save vs. rods, staves, wands
-    save_vs_spell: Type[int]  # Save vs. magic spells and spell-like effects
+    save_vs_ppd: int = 0  # Save vs. poison, paralyzation, death
+    save_vs_pp: int = 0  # Save vs. petrification, polymorph
+    save_vs_bw: int = 0  # Save vs. breath weapon
+    save_vs_rsw: int = 0  # Save vs. rods, staves, wands
+    save_vs_spell: int = 0  # Save vs. magic spells and spell-like effects
 
-    name: str
-    gender: str
-    race: str
-    subrace: str
-    social_class: str
-    alignment: str
-    age: Type[int]
-    height: Type[float]
-    weight: Type[int]
-    special_abilities: List[str]
-    experience: Type[int]
-    level: Type[int]
-    languages: List[str]
+    name: str = ""
+    gender: str = ""
+    race: str = ""
+    subrace: str = ""  # If available, specific type of race, e.g. hill dwarf, high elf, etc.
+    social_class: str = ""
+    alignment: str = ""
+    age: int = 0
+    height: float = 0.0
+    weight: int = 0
+    special_abilities: List[str] = field(default_factory=list)
+    experience: int = 0
+    level: int = 0
+    languages: List[str] = field(default_factory=list)
 
-    armour_class: Type[int]
-    hit_points: Type[int]
-    non_lethal_wounds: Type[int]
-    armour_worn: str
-    init_mod: Type[int]  # Initiative modifier
-    surprise_mod: Type[int]  # Modifier to be surprised
-    attack_column: str  # Attack column (Adv. Dark & Deep only)
-    weapons: Dict[str, int]
+    armour_class: int = 0
+    hit_points: int = 0
+    non_lethal_wounds: int = 0
+    armour_worn: str = ""
+    init_mod: int = 0  # Initiative modifier
+    surprise_mod: int = 0  # Modifier to be surprised
+    attack_column: str = ""  # Attack column (Adv. Dark & Deep only)
+    weapons: Dict[str, int] = field(default_factory=dict)
 
-    skills: Dict[str, int]
-    class_abilities: Dict[str, int]
+    skills: Dict[str, int] = field(default_factory=dict)
+    class_abilities: Dict[str, int] = field(default_factory=dict)
 
-    supplies: Dict[str, int]  # Expendable items
-    equipment: Dict[str, int]  # Non-expendable items
-    encumberance: Type[int]  # Mass of all equipment, supplies, weapons, armour, etc.
-    base_move: Type[int]  # Unencumbered move rate
-    move_rate: Type[int]  # Current move rate
-    magic_items: Dict[str, int]
+    supplies: Dict[str, int] = field(default_factory=dict)  # Expendable items
+    equipment: Dict[str, int] = field(default_factory=dict)  # Non-expendable items
+    encumberance: int = 0  # Mass of all equipment, supplies, weapons, armour, etc.
+    base_move: int = 0  # Unencumbered move rate
+    move_rate: int = 0  # Current move rate
+    magic_items: Dict[str, int] = field(default_factory=dict)
 
-    deeds_titles: str  # Estates, property, and named titles, e.g. Duke
+    deeds_titles: str = ""  # Estates, property, and named titles, e.g. Duke
 
-    mount_name: str
-    mount_type: str
-    mount_hp: Type[int]  # Hit points of mount
-    mount_armour: str
-    mount_ac: Type[int]  # Armour class of mount
+    mount_name: str = ""
+    mount_type: str = ""
+    mount_hp: int = 0  # Hit points of mount
+    mount_armour: str = ""
+    mount_ac: int = 0  # Armour class of mount
 
-    spells_memorized: List[str]
-    spell_components: Dict[str, int]
-    max_spells_memorized: Dict[str, int]  # Maximum number of spells memorized per level
+    spells_memorized: List[str] = field(default_factory=list)
+    spell_components: Dict[str, int] = field(default_factory=dict)
+    max_spells_memorized: Dict[str, int] = field(default_factory=dict) # Maximum number of spells memorized per level
 
     def ability_rolls(self, roll_method):
         """Roll the ability scores for the character."""

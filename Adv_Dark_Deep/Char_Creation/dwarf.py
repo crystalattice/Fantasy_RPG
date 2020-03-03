e7ff9fcea0490a40cd7e2f72effe8a91e6690d17
crystalattice <crystalattice@gmail.com>
2020-03-03 13:26:36 -0600

    Started dwarf-specific character data

--- Adv_Dark_Deep/Char_Creation/dwarf.py
+++ Adv_Dark_Deep/Char_Creation/dwarf.py
@@ -1,2 +1,7 @@
-class Dwarf:
-    pass
+from Adv_Dark_Deep.Char_Creation.basic_character import Character
+
+
+class Dwarf(Character):
+    """Dwarf-specific character characteristics"""
+    def __init__(self):
+        pass

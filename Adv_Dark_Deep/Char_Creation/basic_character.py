f7e59757fdb02aec89c5928258516cba9924979e
crystalattice <crystalattice@gmail.com>
2020-03-06 09:04:55 -0600

    Changed abilities to properties

--- Adv_Dark_Deep/Char_Creation/basic_character.py
+++ Adv_Dark_Deep/Char_Creation/basic_character.py
@@ -8,71 +8,121 @@ from Adv_Dark_Deep.dice_roller import multi_die
 @dataclass()
 class Character:
     """Basic character information, common to all player characters"""
-    # Generic information for all characters
-    strength: float = 0.0
-    dexterity: int = 0
-    intelligence: int = 0
-    wisdom: int = 0
-    constitution: int = 0
-    charisma: int = 0
+    def __init__(self, strength=0.0, dexterity=0, intelligence=0, wisdom=0, constitution=0, charisma=0):
+        # Generic information for all characters
+        self._strength: float = strength
+        self._dexterity: int = dexterity
+        self._intelligence: int = intelligence
+        self._wisdom: int = wisdom
+        self._constitution: int = constitution
+        self._charisma: int = charisma
 
-    save_vs_ppd: int = 0  # Save vs. poison, paralyzation, death
-    save_vs_pp: int = 0  # Save vs. petrification, polymorph
-    save_vs_bw: int = 0  # Save vs. breath weapon
-    save_vs_rsw: int = 0  # Save vs. rods, staves, wands
-    save_vs_spell: int = 0  # Save vs. magic spells and spell-like effects
+        save_vs_ppd: int = 0  # Save vs. poison, paralyzation, death
+        save_vs_pp: int = 0  # Save vs. petrification, polymorph
+        save_vs_bw: int = 0  # Save vs. breath weapon
+        save_vs_rsw: int = 0  # Save vs. rods, staves, wands
+        save_vs_spell: int = 0  # Save vs. magic spells and spell-like effects
 
-    name: str = ""
-    gender: str = ""
+        name: str = ""
+        gender: str = ""
+        race: str = ""
+        dm_underdark = False  # Value to allow DM to allow Underdark (subterranean) races as player characters
 
-    social_class: str = ""
-    alignment: str = ""
-    char_class: Dict[str, int] = field(default_factory=dict)  # Dictionary in case PC is multi/dual classed
-    experience: int = 0
-    level: int = 0
+        social_class: str = ""
+        alignment: str = ""
+        char_class: Dict[str, int] = field(default_factory=dict)  # Dictionary in case PC is multi/dual classed
+        experience: int = 0
+        level: int = 0
 
-    armour_class: int = 0
-    hit_points: int = 0
-    non_lethal_wounds: int = 0
-    armour_worn: str = ""
-    init_mod: int = 0  # Initiative modifier
-    surprise_mod: int = 0  # Modifier to be surprised
-    attack_column: str = ""  # Attack column (Adv. Dark & Deep only)
-    weapons: Dict[str, int] = field(default_factory=dict)
+        armour_class: int = 0
+        hit_points: int = 0
+        non_lethal_wounds: int = 0
+        armour_worn: str = ""
+        init_mod: int = 0  # Initiative modifier
+        surprise_mod: int = 0  # Modifier to be surprised
+        attack_column: str = ""  # Attack column (Adv. Dark & Deep only)
+        weapons: Dict[str, int] = field(default_factory=dict)
 
-    skills: Dict[str, int] = field(default_factory=dict)
-    class_abilities: Dict[str, int] = field(default_factory=dict)
+        skills: Dict[str, int] = field(default_factory=dict)
+        class_abilities: Dict[str, int] = field(default_factory=dict)
 
-    supplies: Dict[str, int] = field(default_factory=dict)  # Expendable items
-    equipment: Dict[str, int] = field(default_factory=dict)  # Non-expendable items
-    encumbrance: int = 0  # Mass of all equipment, supplies, weapons, armour, etc.
-    move_rate: int = 0  # Current move rate
-    magic_items: Dict[str, int] = field(default_factory=dict)
+        supplies: Dict[str, int] = field(default_factory=dict)  # Expendable items
+        equipment: Dict[str, int] = field(default_factory=dict)  # Non-expendable items
+        encumbrance: int = 0  # Mass of all equipment, supplies, weapons, armour, etc.
+        move_rate: int = 0  # Current move rate
+        magic_items: Dict[str, int] = field(default_factory=dict)
 
-    deeds_titles: str = ""  # Estates, property, and named titles, e.g. Duke
+        deeds_titles: str = ""  # Estates, property, and named titles, e.g. Duke
 
-    mount_name: str = ""
-    mount_type: str = ""
-    mount_hp: int = 0  # Hit points of mount
-    mount_armour: str = ""
-    mount_ac: int = 0  # Armour class of mount
+        mount_name: str = ""
+        mount_type: str = ""
+        mount_hp: int = 0  # Hit points of mount
+        mount_armour: str = ""
+        mount_ac: int = 0  # Armour class of mount
 
-    spells_memorized: List[str] = field(default_factory=list)
-    spell_components: Dict[str, int] = field(default_factory=dict)
-    max_spells_memorized: Dict[str, int] = field(default_factory=dict)  # Maximum number of spells memorized per level
+        spells_memorized: List[str] = field(default_factory=list)
+        spell_components: Dict[str, int] = field(default_factory=dict)
+        max_spells_memorized: Dict[str, int] = field(default_factory=dict)  # Maximum number of spells memorized per level
 
-    # Race-specific information
-    race: str = ""
-    dm_underdark = False  # Value to allow DM to allow Underdark (subterranean) races as player characters
-    subrace: str = ""  # If available, specific type of race, e.g. hill dwarf, high elf, etc.
-    age: int = 0
-    height: float = 0.0
-    weight: int = 0
-    special_abilities: List[str] = field(default_factory=list)
-    languages: List[str] = field(default_factory=list)
-    base_move: int = 0  # Unencumbered move rate
-    want_multiclass: bool = False
-    approved_classes: List[str] = field(default_factory=list)
+        # Race-specific information
+        subrace: str = ""  # If available, specific type of race, e.g. hill dwarf, high elf, etc.
+        age: int = 0
+        height: float = 0.0
+        weight: int = 0
+        special_abilities: List[str] = field(default_factory=list)
+        languages: List[str] = field(default_factory=list)
+        base_move: int = 0  # Unencumbered move rate
+        want_multiclass: bool = False
+        approved_classes: List[str] = field(default_factory=list)
+
+    # Properties
+    @property
+    def strength(self) -> float:
+        return self._strength
+
+    @strength.setter
+    def strength(self, value: float) -> None:
+        self._strength = value
+
+    @property
+    def dexterity(self) -> int:
+        return self._dexterity
+
+    @dexterity.setter
+    def dexterity(self, value: int) -> None:
+        self._dexterity = value
+
+    @property
+    def intelligence(self) -> int:
+        return self._intelligence
+
+    @intelligence.setter
+    def intelligence(self, value: int) -> None:
+        self._intelligence = value
+
+    @property
+    def wisdom(self) -> int:
+        return self._wisdom
+
+    @wisdom.setter
+    def wisdom(self, value: int) -> None:
+        self._wisdom = value
+
+    @property
+    def constitution(self) -> int:
+        return self._constitution
+
+    @constitution.setter
+    def constitution(self, value: int) -> None:
+        self._constitution = value
+
+    @property
+    def charisma(self) -> int:
+        return self._charisma
+
+    @charisma.setter
+    def charisma(self, value: int) -> None:
+        self._charisma = value
 
     def ability_rolls(self, roll_method: int) -> None:
         """Roll the ability scores for the character.
@@ -80,20 +130,20 @@ class Character:
         :type roll_method: int
         """
         if roll_method == 1:
-            abil_scores: list = roll_abilities.three_d6()
+            ability_scores: list = roll_abilities.three_d6()
         elif roll_method == 2:
-            abil_scores = roll_abilities.four_d6_drop_lowest()
+            ability_scores = roll_abilities.four_d6_drop_lowest()
         elif roll_method == 3:
-            abil_scores = roll_abilities.two_d6_plus_6()
+            ability_scores = roll_abilities.two_d6_plus_6()
         else:
             raise ValueError("Invalid selection")
 
-        self.strength = abil_scores[0]
-        self.dexterity = abil_scores[1]
-        self.intelligence = abil_scores[2]
-        self.wisdom = abil_scores[3]
-        self.constitution = abil_scores[4]
-        self.charisma = abil_scores[5]
+        self.strength = ability_scores[0]
+        self.dexterity = ability_scores[1]
+        self.intelligence = ability_scores[2]
+        self.wisdom = ability_scores[3]
+        self.constitution = ability_scores[4]
+        self.charisma = ability_scores[5]
 
     def char_name(self, name=None) -> None:
         """Name of the character"""
